351018d64830164bf3ed23b1a7204f7c6bd606bb
mmiillaadddd <milad.jadgal.com@gmail.com>
2016-10-30 09:31:02 -0700

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '274693646:AAFeZvCBTwKndqgRCT4PgONKtTaDH5WM3NE');
+define('BOT_TOKEN', '218356180:AAEiYkK2Yk1M8hb7GxbewYEjPnV8dL6qaQY');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
